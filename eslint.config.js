5f8c1b9b087e0101211f1cb148e6b9a3b8df7f06
Srinidhi Anand <50217774+srinidhi-anand@users.noreply.github.com>
2025-12-30 19:18:45 +0530

    chore: add eslint as dev dependency

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json'
+      }
+    }
+  }
+];
